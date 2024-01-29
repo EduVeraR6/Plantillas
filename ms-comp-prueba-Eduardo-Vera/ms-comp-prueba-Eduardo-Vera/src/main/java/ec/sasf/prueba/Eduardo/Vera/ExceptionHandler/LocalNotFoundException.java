5c53e1101fb69e4c1b5f3d5b8b82f2056613ce2b
Eduardo Vera <edisoneduardoveraromero@gmail.com>
2024-01-29 08:58:53 -0500

    add exceptions

---
+++ ms-comp-prueba-Eduardo-Vera/ms-comp-prueba-Eduardo-Vera/src/main/java/ec/sasf/prueba/Eduardo/Vera/ExceptionHandler/LocalNotFoundException.java
@@ -0,0 +1,9 @@
+package ec.sasf.prueba.Eduardo.Vera.ExceptionHandler;
+
+public class LocalNotFoundException extends Exception {
+
+    public LocalNotFoundException(String message) {
+        super(message);
+    }
+
+}
